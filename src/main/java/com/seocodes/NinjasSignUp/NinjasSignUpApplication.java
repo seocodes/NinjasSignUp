852c961bd82af77d7bb3299ec35196267a5bcb02
seocodes <azanettev@gmail.com>
2025-05-24 20:36:08 -0300

    first commit

---
+++ src/main/java/com/seocodes/NinjasSignUp/NinjasSignUpApplication.java
@@ -0,0 +1,13 @@
+package com.seocodes.NinjasSignUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjasSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjasSignUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seocodes/NinjasSignUp/NinjasSignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seocodes.NinjasSignUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjasSignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
