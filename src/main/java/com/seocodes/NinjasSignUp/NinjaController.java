317c9d1bf01046d6bdbf2616d28c79fd614da7f9
seocodes <azanettev@gmail.com>
2025-05-24 23:42:06 -0300

    Cria uma entidade no banco de dados a partir do NinjaModel

--- src/main/java/com/seocodes/NinjasSignUp/NinjaController.java
+++ src/main/java/com/seocodes/NinjasSignUp/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/welcome")
     public String welcome(){
         return "This is my first message in this route!";
