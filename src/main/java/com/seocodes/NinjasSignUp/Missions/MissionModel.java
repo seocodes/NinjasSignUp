55a1375d31ac608d9f80608f951b6544a2ae32ee
seocodes <azanettev@gmail.com>
2025-05-29 22:27:20 -0300

    Lombok added

--- src/main/java/com/seocodes/NinjasSignUp/Missions/MissionModel.java
+++ src/main/java/com/seocodes/NinjasSignUp/Missions/MissionModel.java
@@ -2,26 +2,25 @@ package com.seocodes.NinjasSignUp.Missions;
 
 import com.seocodes.NinjasSignUp.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_mission")
+@NoArgsConstructor
+@AllArgsConstructor
+//faz os getters e setters
+@Data
 public class MissionModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String dificuldade;
-
     //@OneToMany - uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "missions")
     private List<NinjaModel> ninjas;
-
-    public MissionModel(){}
-
-    public MissionModel(String nome, String dificuldade){
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
 }
--- src/main/java/com/seocodes/NinjasSignUp/Ninjas/NinjaModel.java
+++ src/main/java/com/seocodes/NinjasSignUp/Ninjas/NinjaModel.java
@@ -2,6 +2,9 @@ package com.seocodes.NinjasSignUp.Ninjas;
 
 import com.seocodes.NinjasSignUp.Missions.MissionModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,6 +12,10 @@ import java.util.List;
 //JPA = Java Persistence API
 @Entity
 @Table(name="tb_signup")
+@NoArgsConstructor
+@AllArgsConstructor
+//faz os getters e setters
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,41 +23,8 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
-
     //@ManyToOne - um ninja tem uma única missão
     @ManyToOne
     @JoinColumn(name = "missions_id") //FOREIGN KEY!!!
     private MissionModel missions;
-
-    public NinjaModel(){}
-
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
